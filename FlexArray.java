88bb2e1785d23bdca8b4fdaa6b53853dd1e87c53
Valuka gimhan <valukagimhan@gmail.com>
2022-11-26 09:14:32 +0530

    2

--- FlexArray.java
+++ FlexArray.java
@@ -4,55 +4,55 @@ package FLEXARRAY;
 public class FlexArray {
         
     //Declare an array
-    private int[] anArray; 
+    private int[] arr0; 
     
     //Creating a constructor to initializes the array object
-    public FlexArray(int[] anArray){
-        this.anArray = anArray;
+    public FlexArray(int[] arr0){
+        this.arr0 = arr0;
     }
     
     /*Creating a method to add a element to a array if there is a
       position last*/
     public void insert(int insertValue){
             this.increase();
-            anArray[anArray.length - 1 ] = insertValue;
+            arr0[arr0.length - 1 ] = insertValue;
 
     }
     
     // Creating a method to delete an element of array by using index
     public void deletByIndex(int deleteIndex){
         // temporary array to hold values without deleted index
-         int [] arrayTempDel = new int [anArray.length - 1];
+         int [] arrTmpDel = new int [arr0.length - 1];
 
-        // copy elements from original anArray from beginning till deleteIndex into arrayTempdel
-        System.arraycopy(anArray, 0, arrayTempDel, 0, deleteIndex); 
+        // copy elements from original arr0 from beginning till deleteIndex into arrTmpDel
+        System.arraycopy(arr0, 0, arrTmpDel, 0, deleteIndex); 
  
-        // copy elements from anArray from deleteIndex+1 till end into arrayTempdel
-        System.arraycopy(anArray, deleteIndex+1, arrayTempDel, deleteIndex, anArray.length - deleteIndex-1);
+        // copy elements from arr0 from deleteIndex+1 till end into arrTmpDel
+        System.arraycopy(arr0, deleteIndex+1, arrTmpDel, deleteIndex, arr0.length - deleteIndex-1);
 
-        anArray = arrayTempDel;
+        arr0 = arrTmpDel;
 
     }
     
     // Creating a method to delete an element with it's value
     public void deletByValue(int deleteValue){
-        for(int i=0; i<anArray.length; i++){
-            if(anArray[i] == deleteValue){
+        for(int i=0; i<arr0.length; i++){
+            if(arr0[i] == deleteValue){
                 // temporary array to hold values without deleted index
-            int [] arrayTempDel = new int [anArray.length - 1];
+            int [] arrTmpDel = new int [arr0.length - 1];
 
-            // copy elements from original anArray from beginning till deleteIndex into arrayTempdel
-            System.arraycopy(anArray, 0, arrayTempDel, 0, i); 
+            // copy elements from original arr0 from beginning till deleteIndex into arrTmpDel
+            System.arraycopy(arr0, 0, arrTmpDel, 0, i); 
   
-            // copy elements from anArray from deleteIndex+1 till end into arrayTempdel
-            System.arraycopy(anArray, i+1, arrayTempDel, i, anArray.length - i-1);
+            // copy elements from arr0 from deleteIndex+1 till end into arrTmpDel
+            System.arraycopy(arr0, i+1, arrTmpDel, i, arr0.length - i-1);
  
-            anArray = arrayTempDel;
+            arr0 = arrTmpDel;
 
                 break; 
             }
 
-            else if (i>=anArray.length){
+            else if (i>=arr0.length){
                 System.out.println("Cound't find the value" + deleteValue + "in index" + i);
                 break; 
             }
@@ -61,25 +61,25 @@ public class FlexArray {
    
     // Creating a method to update a value of an element in array
    public void update(int updateIndex, int updateValue){
-       anArray[updateIndex] = updateValue;
+       arr0[updateIndex] = updateValue;
    }
      
     //Creating a method to display all the elements of array
     public void display(){
-        for(int i=0; i<anArray.length; i++){
-            System.out.print(anArray[i] + "\t"); 
+        for(int i=0; i<arr0.length; i++){
+            System.out.print(arr0[i] + "\t"); 
         }
     }
     
     
     //Creating a method to find a element in array
     public void search(int searchKey){
-        for(int i=0; i<anArray.length; i++){
-            if(anArray[i] == searchKey){
+        for(int i=0; i<arr0.length; i++){
+            if(arr0[i] == searchKey){
                 System.out.println("Found" + searchKey + "in index" + i);
                 break; 
             }
-            else if(i == anArray.length){
+            else if(i == arr0.length){
                 System.out.println("Cound't find the value" + searchKey + "in index" + i);
                 break; 
             }
@@ -90,36 +90,36 @@ public class FlexArray {
     
     //Creating a method to increase the array size for insert values
     public void increase(){
-        int[] anArrayTemp = new int[anArray.length + 1];
-        System.arraycopy(anArray, 0, anArrayTemp, 0, anArray.length);
-        anArray = anArrayTemp;
+        int[] arr0Temp = new int[arr0.length + 1];
+        System.arraycopy(arr0, 0, arr0Temp, 0, arr0.length);
+        arr0 = arr0Temp;
     }
 
     //sort the array in acending order method
     public void sortAsc(){
        
         //sort by using insertion sort
-        for (int mainLoop = 0; mainLoop < anArray.length; mainLoop++) {
-            int temp = anArray[mainLoop];   //remove the value
+        for (int mainLoop = 0; mainLoop < arr0.length; mainLoop++) {
+            int temp = arr0[mainLoop];   //remove the value
             int searchLoop=mainLoop;    //start from main loop
-            while (searchLoop>0 && anArray[searchLoop-1] >= temp) {
-                anArray[searchLoop]=anArray[searchLoop-1];  //shifting to right
+            while (searchLoop>0 && arr0[searchLoop-1] >= temp) {
+                arr0[searchLoop]=arr0[searchLoop-1];  //shifting to right
                 searchLoop--;
             }
-            anArray[searchLoop] = temp; //insert the value in relevent place
+            arr0[searchLoop] = temp; //insert the value in relevent place
         }
     }
     //sort the array in decending order method
     public void sortDes(){
         //sort by using insertion sort
-        for (int mainLoop = 1; mainLoop < anArray.length; mainLoop++) {
-            int temp = anArray[mainLoop];  //remove the value
+        for (int mainLoop = 1; mainLoop < arr0.length; mainLoop++) {
+            int temp = arr0[mainLoop];  //remove the value
             int searchLoop=mainLoop;    //start from main loop
-            while (searchLoop>0 && anArray[searchLoop-1] <= temp) {
-                anArray[searchLoop]=anArray[searchLoop-1]   ;//shifting to right
+            while (searchLoop>0 && arr0[searchLoop-1] <= temp) {
+                arr0[searchLoop]=arr0[searchLoop-1]   ;//shifting to right
                 searchLoop--;
             }
-            anArray[searchLoop] = temp;//insert the value in relevent place
+            arr0[searchLoop] = temp;//insert the value in relevent place
         }
     }
 }   
--- worker.java
+++ worker.java
@@ -14,18 +14,18 @@ public class TestFlexArray {
         int len = scan.nextInt();
 
         //Create a temporary array for add elements to a new array
-        int[] anArrayTemp = new int[len];
+        int[] arrTmp = new int[len];
 
         //Get elements of array
         for(int i=0; i<5; i++){
             System.out.println("Enter a number for index : " + i );
-            anArrayTemp[i] = scan.nextInt();
+            arrTmp[i] = scan.nextInt();
         }
 
         System.out.println("\n");
 
         //Assign the elements of temporary array into new array
-        FlexArray anArray01 = new FlexArray(anArrayTemp);
+        FlexArray arr1 = new FlexArray(arrTmp);
 
         // boolean trigger = true;
         // while(trigger){
@@ -42,47 +42,47 @@ public class TestFlexArray {
 
         // }
        
-        anArray01.display();
+        arr1.display();
         System.out.println("\n");
 
         //Calling the insert method from flexArray class
         System.out.println("Enter a insertValue:" );
         Scanner scan = new Scanner(System.in);
         int insertValue = scan.nextInt();
-        anArray01.insert(insertValue);
-        anArray01.display();
+        arr1.insert(insertValue);
+        arr1.display();
         System.out.println("\n");
 
         
         System.out.println("Asending order of the array\n");
-        anArray01.sortAsc();
-        anArray01.display();
+        arr1.sortAsc();
+        arr1.display();
         System.out.println("\n");
         
         System.out.println("Decending order of the array\n");
-        anArray01.sortDes();
-        anArray01.display();
+        arr1.sortDes();
+        arr1.display();
         System.out.println("\n");
         
         
         //Calling the deleteByIndex method from flexArray class
         System.out.println("Enter a deleteIndex:" );
         int deleteIndex = scan.nextInt();
-        anArray01.deletByIndex(deleteIndex);
-        anArray01.display();
+        arr1.deletByIndex(deleteIndex);
+        arr1.display();
         System.out.println("\n");
 
         //Calling the deleteByValue method from flexArray class
         System.out.println("Enter a deleteValue:" );
         int deleteValue = scan.nextInt();
-        anArray01.deletByValue(deleteValue);
-        anArray01.display();
+        arr1.deletByValue(deleteValue);
+        arr1.display();
         System.out.println("\n");
 
         //Calling the searchKey method from flexArray class
         System.out.println("Enter a searchKey:" );
         int searchKey = scan.nextInt();
-        anArray01.search(searchKey);
+        arr1.search(searchKey);
         System.out.println("\n");
 
         //Calling the update method from flexArray class
@@ -90,8 +90,8 @@ public class TestFlexArray {
         int updateValue = scan.nextInt();
         System.out.println("Enter a updateIndex:" );
         int updateIndex = scan.nextInt();
-        anArray01.update(updateIndex, updateValue);
-        anArray01.display();
+        arr1.update(updateIndex, updateValue);
+        arr1.display();
         System.out.println("\n");
 
 
